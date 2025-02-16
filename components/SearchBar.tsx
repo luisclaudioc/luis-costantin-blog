0fe29f8d93770ffa2f3cf7209a57a280f71eb2f0
Luis Costantin <luisclaudioc@hotmail.com>
2025-02-16 13:22:29 +0000

    search bar responsiviness

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,15 +6,15 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ searchQuery, setSearchQuery }: SearchBarProps) {
-    return (
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Search posts..."
-        className="w-96 px-4 py-2 border border-gray-500 rounded-full bg-gray-700 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-    );
+  return (
+    <input
+      type="text"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+      className="w-full sm:w-72 md:w-96 px-4 py-2 border border-gray-500 rounded-full bg-gray-700 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
 }
   
   
